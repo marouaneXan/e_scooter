16dd3d8aaf539802efaa45f43f2954556c44ddc7
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-15 11:43:27 +0100

    redirect to the map screen

--- navigatores/index.jsx
+++ navigatores/index.jsx
@@ -1,12 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
+import Map from '../screens/Map';
 const Stack = createNativeStackNavigator();
 const RootStack = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name='Home' component={Home} />
+                <Stack.Screen name='Map' component={Map} />
             </Stack.Navigator>
         </NavigationContainer>
     )
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 import tw from 'twrnc';
 import { Icon } from '@rneui/themed';
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-6`}>
         <Text style={tw`text-2xl font-bold`}>E_scooter</Text>
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 rounded-[8px]`}>
+        <TouchableOpacity onPress={()=>navigation.navigate('Map')} style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 rounded-[8px]`}>
           <View>
             <Image style={{ width: 120, height: 120, resizeMode: 'contain' }} source={{ uri: 'https://links.papareact.com/3pn' }} />
             <Text style={tw`mt-2 text-lg font-semibold`}>Get a ride</Text>

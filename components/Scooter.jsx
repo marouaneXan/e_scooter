8eedeb5c7583ff991a4c02099d8e9a4c45518a4b
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-22 20:15:29 +0100

    create first version for scooter component

---
+++ components/Scooter.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Scooter = () => {
+  return (
+    <View>
+      <Text>Scooter</Text>
+    </View>
+  )
+}
+
+export default Scooter

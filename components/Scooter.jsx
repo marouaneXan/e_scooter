69993b7db31d8d328a16942cc3fbc21aaa4f473e
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-22 20:20:07 +0100

    map throw scooters

--- components/Scooter.jsx
+++ components/Scooter.jsx
@@ -1,12 +1,40 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
+import { Marker } from "react-native-maps";
+import axios from "axios";
+import Constants from "expo-constants";
 
 const Scooter = () => {
-  return (
-    <View>
-      <Text>Scooter</Text>
-    </View>
-  )
+    const [scooters, setScooters] = useState([]);
+    const [id, setId] = useState(null);
+    const apiUrl = Constants.expoConfig.extra.apiUrl;
+
+    useEffect(() => {
+        axios
+            .get(`${apiUrl}/user/allScooters`)
+            .then((response) => {
+                setScooters(response.data);
+                setId(response.data._id);
+            })
+            .catch((error) => console.log("error " + error));
+    }, []);
+    return (
+        <View>
+            {scooters.map((scooter) => (
+                <Marker
+                    key={scooter._id}
+                    coordinate={{
+                        latitude: scooter.latitude,
+                        longitude: scooter.longitude,
+                    }}
+                    onPress={() => {
+                        onScooterPress(scooter), storeId(scooter._id);
+                    }}
+                />
+            ))}
+        </View>
+    )
 }
 
 export default Scooter

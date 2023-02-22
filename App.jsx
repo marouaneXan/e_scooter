50a65a6014051657bfbc1b2ee44c026ff0acd3d8
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-22 20:11:43 +0100

    some changes in app compnent

--- App.jsx
+++ App.jsx
@@ -1,24 +1,8 @@
 import { StyleSheet } from 'react-native';
-import { Provider } from 'react-redux'
-import { store } from './app/store';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootStack from './navigatores';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <SafeAreaProvider>
-        <RootStack />
-      </SafeAreaProvider>
-    </Provider>
+    <RootStack />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import navReducer from '../features/navSlice.js'
-export const store = configureStore({
-    reducer: {
-        nav:navReducer
-    }
-});

ef31952eb8e7b1ab5cd3db7086ee759519f5434a
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-22 20:09:15 +0100

    install react-native-map

--- screens/Map.jsx
+++ screens/Map.jsx
@@ -1,12 +1,1 @@
-import { View, Text } from 'react-native'
-import React from 'react'
 
-const Map = () => {
-  return (
-    <View>
-      <Text>Map screen ...</Text>
-    </View>
-  )
-}
-
-export default Map

41e7a95a63ca5d81dbd3c0ac8c2ec0a9b2286ffe
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-27 09:07:29 +0100

    import RegisterScreen

--- navigatores/index.jsx
+++ navigatores/index.jsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import Map from '../screens/Map';
+import RegisterScreen from '../screens/RegisterScreen';
 const Stack = createNativeStackNavigator();
 const RootStack = () => {
     return (
